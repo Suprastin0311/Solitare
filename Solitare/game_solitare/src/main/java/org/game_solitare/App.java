5f0e4014a0edce8deb8fbcf559a8c23546132f66
Suprastin0311 < @dd_nikolaenko>
2025-03-10 10:26:54 +0300

    Создание модуля game_solitare

---
+++ Solitare/game_solitare/src/main/java/org/game_solitare/App.java
@@ -0,0 +1,10 @@
+package org.game_solitare;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Module Game_Solitare");
+    }
+}
